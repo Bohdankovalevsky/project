2624bb94b9695f0027cee6fd89c74531529356a0
Leshchenko <denys.leshchenko@oa.edu.ua>
2022-05-14 17:20:00 +0300

    repo

---
+++ LiquidWebApp/Controllers/LiquidController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LiquidRepositories;
+using LiquidCore;
+
+namespace LiquidWebApp.Controllers
+{
+    public class LiquidController : Controller
+    {
+        private readonly LiquidInfoRepository _liquidInfoRepository;
+        public LiquidController(LiquidInfoRepository liquidInfoRepository)
+        {
+            _liquidInfoRepository = liquidInfoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View(new LiquidINfo());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LiquidINfo model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _liquidInfoRepository.CreateAsync(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
